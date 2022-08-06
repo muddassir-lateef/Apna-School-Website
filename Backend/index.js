cb37f604ce6d6609a4f2a7fd811156ef18005f56
Salar24 <85688754+Salar24@users.noreply.github.com>
2022-08-07 04:30:15 +0500

    Added backend get/post with thunderclient

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,28 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose')
+const AdminModel = require('./models/Admin');
+mongoose.connect("mongodb+srv://Salar:ApnaSchool@apnaschool.y3ountm.mongodb.net/Apna-School-DB?retryWrites=true&w=majority");
+app.use(express.json());
+app.use(cors());
 
+app.get("/getAdmins", (req,res) => {
+    AdminModel.find({}, (err,result) => {
+        if (err) {
+            res.json(err)
+        } else {
+            res.json(result)
+        }
+    });
+});
+
+app.post("/addAdmin", async (req,res) => {
+    const object = req.body;
+    const newObject = new AdminModel(object);
+    await newObject.save();
+
+    res.json(object);
+})
 app.listen(3001, () => {
     console.log("Zuberi is beauitful very")
 })
