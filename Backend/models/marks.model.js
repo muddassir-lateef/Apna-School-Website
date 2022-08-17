86c5c3efb3429451e8a19f0bf865fba6e850ec54
Shayan Amir <74527163+ShayanZuberi@users.noreply.github.com>
2022-08-17 20:55:05 +0500

    Added exam, marks, lecture and calendar modules

---
+++ Backend/models/marks.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+let Student = require('../models/student.model');
+let Exam = require('../models/exam.model');
+
+const marksSchema = new Schema({
+  
+  marksID: {
+    type: String,  
+    required: true
+  },
+
+  obtainedMarks:{
+    type: Number,
+    required: true
+  },
+
+  studentId:{
+    type:Schema.Types.ObjectId ,ref:Student
+    
+  },
+
+  //Exam to which the marks belong
+  examId:{
+    type:Schema.Types.ObjectId ,ref:Exam
+  }
+
+},
+ {
+  timestamps: true
+});
+
+const Marks = mongoose.model('Marks', marksSchema);
+
+module.exports = Marks;
