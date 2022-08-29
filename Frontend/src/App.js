89962f23d1a11338a80305652c1cffef1fbf2f4f
Shayan Amir <74527163+ShayanZuberi@users.noreply.github.com>
2022-08-29 15:53:29 +0500

    Implemented new frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,62 +1,11 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Navigate, Routes} from 'react-router-dom';
-import React, { useState, useCallback } from 'react';
-
-import { AuthContext } from './shared/context/auth-context';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import Auth from './staff/pages/Auth';
-import Students from './students/pages/Students';
-import NewStudent from './students/pages/NewStudent';
-import NewTeacher from './teachers/pages/NewTeacher';
-import StudentInfo from './students/pages/StudentInfo';
-import UpdateStudent from './students/pages/UpdateStudent';
+import SignIn from'./pages/SignIn/Signin';
 
 function App() {/////
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const login = useCallback(()=>{
-    setIsLoggedIn(true);
-  }, []);
-  const logout = useCallback(()=>{
-    setIsLoggedIn(false);
-  }, []);
-
-  let routes;
-  if(isLoggedIn){
-    routes = (
-      <Routes>
-        <Route path="/" element={<Navigate to="/students" />}></Route>
-        <Route path="/auth" element={<Auth />}></Route>
-        <Route path="/students" element={<Students/>}></Route>
-        <Route path="/student/new" element={<NewStudent/>}></Route>
-        <Route path="/teacher/new" element={<NewTeacher/>}></Route>
-        <Route path="/student/:studentId" element={<StudentInfo/>}></Route>
-        <Route path="/updateStudent/:studentId" element={<UpdateStudent/>}></Route>
-
-      </Routes>
-    );
-  }
-  else{
-    routes = (
-      <Routes>
-        <Route path="/" element={<Navigate to="/auth" />}></Route>
-        <Route path="/auth" element={<Auth />}></Route>
-      </Routes>
-    );
-  }
   return (
-    <AuthContext.Provider
-      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
-    >
-      <Router>
-        <div className="app-global-styles">
-          <MainNavigation />
-          <main>{routes}</main>
-        </div>
-      </Router>
-    </AuthContext.Provider>
+    <SignIn/>
   );
 }
-
 
 
 export default App;
