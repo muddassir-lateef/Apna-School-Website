8951cbc9a5ad51a7d5158fb1524eed88898a5e80
Muddassir Lateef <muddassirlateef001@gmail.com>
2022-09-06 16:29:39 +0500

    Fix: Drawer fixed to appear below appbar correctly

--- Frontend/src/components/NavBar.js
+++ Frontend/src/components/NavBar.js
@@ -1,31 +1,31 @@
 import { AppBar, styled, Toolbar, Typography, Box, Button, Drawer, List, ListItem, CssBaseline, ListItemIcon, ListItemText } from "@mui/material";
 import LogoutIcon from '@mui/icons-material/Logout';
 import SchoolIcon from '@mui/icons-material/School';
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { theme } from "../Themes/Default-theme";
 import MenuIcon from '@mui/icons-material/Menu';
 import IconButton from '@mui/material/IconButton';
-import { AuthContext} from '../../src/context/AuthContext';
+import { AuthContext } from '../../src/context/AuthContext';
 import Face6Icon from '@mui/icons-material/Face6';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 const drawerWidth = 240;
 const menuItems = [
-    {
-        title: "All students",
-        path: '/students',
-        icon: <Face6Icon/>
-    },
-    {
-        title: "All teachers",
-        path: '/teachers',
-        icon: <Face6Icon/>
-    },
-    {
-        title: "Calendar",
-        path: '/calendar',
-        icon: <CalendarMonthIcon/>
-    }
+  {
+    title: "All students",
+    path: '/students',
+    icon: <Face6Icon />
+  },
+  {
+    title: "All teachers",
+    path: '/teachers',
+    icon: <Face6Icon />
+  },
+  {
+    title: "Calendar",
+    path: '/calendar',
+    icon: <CalendarMonthIcon />
+  }
 ]
 const StyledToolbar = styled(Toolbar)({
   backgroundColor: theme.palette.primary.main,
@@ -40,55 +40,58 @@ const Icons = styled(Box)(({ theme }) => ({
 }));
 
 const LogoutButton = styled(Button)(({ theme }) => ({
-    backgroundColor: theme.palette.primary.main,
-    "&:hover": {
-        backgroundColor: "red"
-      }    
+  backgroundColor: theme.palette.primary.main,
+  "&:hover": {
+    backgroundColor: "red"
+  }
 }));
 
 
 
 
 const NavBar = () => {
-    const auth = useContext(AuthContext);
+  const auth = useContext(AuthContext);
 
-    const logoutHandler = () => {
-        auth.logout();
-    }
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+  const logoutHandler = () => {
+    auth.logout();
+  }
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-    const handleDrawerToggle = () => {
-      setMobileOpen(!mobileOpen);
-    };
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
   return (
     <div>
-        <CssBaseline/>
-    <AppBar position = "sticky">
-      <StyledToolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          onClick={handleDrawerToggle}
-          sx={{ mr: 2, display: { sm: "none" } }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Icons>
-          <SchoolIcon />
-          <Typography variant="h6">Apna School</Typography>
-        </Icons>
+      <AppBar sx={{
+        position: 'relative',
+        zIndex: theme.zIndex.drawer + 1
+      }}
+      >
+        <StyledToolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Icons>
+            <SchoolIcon />
+            <Typography variant="h6">Apna School</Typography>
+          </Icons>
 
-        <LogoutButton variant="contained" onClick={logoutHandler}>
-          <LogoutIcon />
-        </LogoutButton>
-      </StyledToolbar>
+          <LogoutButton variant="contained" onClick={logoutHandler}>
+            <LogoutIcon />
+          </LogoutButton>
+        </StyledToolbar>
 
-      
-    </AppBar>
 
-    <Drawer
+      </AppBar>
+
+      <Drawer
         variant="permanent"
         sx={{
           display: { xs: "none", sm: "block" },
@@ -100,15 +103,15 @@ const NavBar = () => {
           },
         }}
       >
+        <Toolbar />
         <List>
-            {menuItems.map(item => (
-                <ListItem button key={item.title} >
-                    <ListItemIcon>{item.icon}</ListItemIcon>
-                    <ListItemText primary={item.title}/>
-                </ListItem>
-            ))}
+          {menuItems.map(item => (
+            <ListItem button key={item.title} >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.title} />
+            </ListItem>
+          ))}
         </List>
-        
       </Drawer>
 
     </div>
