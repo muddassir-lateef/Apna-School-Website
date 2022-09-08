35ec0ab222476e5bfc7efb0d173d742b4a24bdba
Muddassir Lateef <muddassirlateef001@gmail.com>
2022-09-08 14:30:04 +0500

    Side Bar with nested menu added

--- Frontend/src/components/SDrawer.js
+++ Frontend/src/components/SDrawer.js
@@ -1,40 +1,75 @@
-import {Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar } from "@mui/material";
+import { Drawer, List, ListItem, ListItemIcon, ListItemButton, ListItemText, Toolbar, Collapse, ListSubheader } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 
 import React from "react";
 
 import Face6Icon from '@mui/icons-material/Face6';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-
+import NestedList from "./NestedList";
+import Divider from '@mui/material/Divider';
 const drawerWidth = 240;
 const menuItems = [
-    {
-        title: "All students",
-        path: '/admin/students',
-        icon: <Face6Icon/>
+  {
+    
+    menuTitle: "Student",
+    enteries: [{
+      title: "All students",
+      path: '/admin/students',
+      icon: <Face6Icon />
     },
     {
-        title: "All teachers",
-        path: '/admin/teachers',
-        icon: <Face6Icon/>
+      title: "All teachers",
+      path: '/admin/teachers',
+      icon: <Face6Icon />
     },
     {
-        title: "Calendar",
-        path: '/admin/calendar',
-        icon: <CalendarMonthIcon/>
+      title: "Calendar",
+      path: '/admin/calendar',
+      icon: <CalendarMonthIcon />
     }
+    ]
+  },
+
+  {
+    menuTitle: "Teacher",
+    enteries: [{
+      title: "All students",
+      path: '/admin/students',
+      icon: <Face6Icon />
+    },
+    {
+      title: "All teachers",
+      path: '/admin/teachers',
+      icon: <Face6Icon />
+    },
+    {
+      title: "Calendar",
+      path: '/admin/calendar',
+      icon: <CalendarMonthIcon />
+    }
+    ]
+  }
+
 ]
+
 
 const SDrawer = (props) => {
   const navigate = useNavigate();
 
-  const {mobileOpen}=props;
-  const {handleDrawerToggle}=props;
-  
+  const { mobileOpen } = props;
+  const { handleDrawerToggle } = props;
+
   const handleSideBarClick = (path) => {
     if (mobileOpen) handleDrawerToggle();
     navigate(path, { replace: true });
   }
+  const [studentMenuOpen, setStudentMenuOpen] = React.useState(true);
+
+  const handleStudentMenuClick = () => {
+    setStudentMenuOpen(!studentMenuOpen);
+  };
 
   return (
     <div>
@@ -55,13 +90,30 @@ const SDrawer = (props) => {
         }}
       >
         <Toolbar />
-        <List>
-            {menuItems.map(item => (
-                <ListItem button key={item.title} onClick={() => handleSideBarClick(item.path)}>
-                    <ListItemIcon>{item.icon}</ListItemIcon>
-                    <ListItemText primary={item.title}/>
-                </ListItem>
-            ))}
+        <List >
+
+          {menuItems.map((menu) => (
+            <>
+              <ListItemButton onClick={handleStudentMenuClick}>
+                <ListItemText primary={menu.menuTitle} />
+                {studentMenuOpen ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={studentMenuOpen} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+
+                  {menu.enteries.map(item => (
+                    <>
+                      <ListItem button key={item.title} onClick={() => handleSideBarClick(item.path)} sx={{ pl: 4 }}>
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText primary={item.title} />
+                      </ListItem>
+                    </>
+                  ))}
+                </List>
+              </Collapse>
+            </>
+          ))}
+
         </List>
       </Drawer>
       <Drawer
@@ -77,16 +129,33 @@ const SDrawer = (props) => {
         }}
       >
         <Toolbar />
-        <List>
-            {menuItems.map(item => (
-                <ListItem button key={item.title} onClick={() => handleSideBarClick(item.path)}>
-                    <ListItemIcon>{item.icon}</ListItemIcon>
-                    <ListItemText primary={item.title}/>
-                </ListItem>
-            ))}
+        <List >
+
+          {menuItems.map((menu) => (
+            <>
+              <ListItemButton onClick={handleStudentMenuClick}>
+                <ListItemText primary={menu.menuTitle} />
+                {studentMenuOpen ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={studentMenuOpen} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+
+                  {menu.enteries.map(item => (
+                    <>
+                      <ListItem button key={item.title} onClick={() => handleSideBarClick(item.path)} sx={{ pl: 4 }}>
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText primary={item.title} />
+                      </ListItem>
+                    </>
+                  ))}
+                </List>
+              </Collapse>
+            </>
+          ))}
+
         </List>
       </Drawer>
- 
+
     </div>
   );
 };
